7d8685481e7bf03715be82e6a5a00aec98a07e18
Jonathan Souza <souzajonathan570@gmail.com>
2023-05-12 17:50:35 -0300

    fix: fix import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,35 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from 'styles/global'
+import theme from 'styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
-        <title>Boilerplate-Next</title>
+        <title>SpendSmart</title>
         <link rel="shortcut ico" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
         <meta
           name="description"
-          content="A simple project starter to work with Typescript, React, NextJs and Styled Component"
+          content="Um Web App de gerenciamento de finnanças."
         />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
